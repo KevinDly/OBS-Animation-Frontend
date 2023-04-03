d757f1518e20f0038111a000349b49a352d01422
Saibogu <kdolay@ucsd.edu>
2023-04-03 04:27:15 -0700

    Added code for EmoteButtons and EmoteContainers, added code directly for Twitch API calls

--- src/App.js
+++ src/App.js
@@ -1,25 +1,161 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./App.css"
+import EmotePickerContainer from './components/EmotePickerContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const WEBSOCKET_URL = "ws://localhost:2999"
+const WEBSOCKET_PROTOCOLS = ["streamerController"]
+const DATA_SEND_TYPE = "executeAnimation"
+
+const TWITCH_OAUTH_URL = 'https://id.twitch.tv/oauth2/token'
+const TWITCH_GLOBAL_EMOTES_URL = 'https://api.twitch.tv/helix/chat/emotes/global'
+const TWITCH_VALIDATION_URL = 'https://id.twitch.tv/oauth2/validate'
+
+//TODO: Put in a separate file.
+const TWITCH_CLIENT_SECRET = 'vh3a92zgt8617h0er1ohymnrn71nlt'
+const TWITCH_CLIENT_ID = 'bmkhxh3eb8cl8uvtkwm6fbrahzgkdx'
+
+const emoteJSONArray = [
+    {imgSrc: "https://i.kym-cdn.com/photos/images/original/001/923/849/90f",
+    imgName: "AYAYA"},
+    {imgSrc: "https://i.kym-cdn.com/photos/images/original/001/923/849/90f",
+    imgName: "AYAYA"},
+    {imgSrc: "https://i.kym-cdn.com/photos/images/original/001/923/849/90f",
+    imgName: "AYAYA"},
+    {imgSrc: "https://i.kym-cdn.com/photos/images/original/001/923/849/90f",
+    imgName: "AYAYA"}
+]
+
+class App extends Component {
+
+    constructor(props) {
+        super(props)
+        this.websocket = new WebSocket(WEBSOCKET_URL, WEBSOCKET_PROTOCOLS)
+        this.sendData = this.sendData.bind(this)
+        this.updateData = this.updateData.bind(this)
+        this.sendEmoteButton = this.sendEmoteButton.bind(this)
+
+        this.state = {
+            emoteDensity: 0,
+            imageURL: "",
+            emoteArray: emoteJSONArray,
+            didConnect: false,
+            didAuthenticate: false
+        }
+    }
+
+    getEmotes({access_token, expiration, token_type}) {
+        /*fetch(TWITCH_VALIDATION_URL, {
+            headers: {
+                'Authorization': 'OAuth ' + access_token
+            }
+        })*/
+
+        if(this.state.didConnect) {
+            console.log("Connected")
+            return
+        }
+        else {
+            fetch(TWITCH_GLOBAL_EMOTES_URL, {
+                headers: {
+                    'Authorization': 'Bearer ' + access_token,
+                    'Client-Id': TWITCH_CLIENT_ID
+                }
+            })
+            .then(response => response.json())
+            .then(response => {
+                this.setState({emoteArray: response.data.map(data => ({
+                    imgSrc: data.images.url_4x,
+                    imgName: data.name
+                }))
+            })})
+            this.setState({didConnect: true})
+        }
+    }
+
+    componentDidMount(){
+        console.log("Mounted") 
+
+        if(this.state.didAuthenticate) {
+            console.log("Already authenticated")
+            return
+        }
+        fetch(TWITCH_OAUTH_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams({
+                'client_id': TWITCH_CLIENT_ID,
+                'client_secret': TWITCH_CLIENT_SECRET,
+                'grant_type': 'client_credentials'
+            })
+        })
+        .then(response => response.json())
+        .then(response => {
+            this.getEmotes(response)
+            this.setState({didAuthenticate: true})
+        })
+    }
+
+    sendData() {
+        const emoteData = {
+            type: DATA_SEND_TYPE,
+            data: {
+                url: this.state.imageURL,
+                emoteDensity: this.state.emoteDensity
+            }
+        }
+
+        console.log(emoteData)
+        try{
+            console.log(this.websocket)
+            const stringifiedData = JSON.stringify(emoteData)
+            console.log(stringifiedData)
+            this.websocket.send(stringifiedData)
+        }
+        catch(error) {
+            console.log(error)
+        }
+    }
+
+    sendEmoteButton(src, name) {
+
+        console.log(src)
+        const emoteData = {
+            type: DATA_SEND_TYPE,
+            data: {
+                url: src,
+                emoteDensity: document.getElementById("emoteDensityInput").value
+            }
+        }
+
+        console.log(emoteData)
+        try{
+            console.log(this.websocket)
+            const stringifiedData = JSON.stringify(emoteData)
+            console.log(stringifiedData)
+            this.websocket.send(stringifiedData)
+        }
+        catch(error) {
+            console.log(error)
+        }
+    }
+
+    updateData(e, type) {
+        console.log(e.target.value)
+        this.setState({[type]: e.target.value})
+    }
+
+    //TODO: Prevent jsonarray from reupdating every time something else on the page updates.
+
+    render() {
+        return <div id = "page_div">
+            <input type="number" id="emoteDensityInput"></input>
+            <input type="text" id="emoteURLInput" onChange = { (e) => this.updateData(e, "imageURL") }></input>
+            <button type="button" id="emoteDataSubmit" onClick={ this.sendData }>Submit</button>
+            <EmotePickerContainer emoteJSONArray={this.state.emoteArray} onClickEmote = { this.sendEmoteButton }/>
+        </div>
+    }
 }
 
-export default App;
+export default App
---
+++ src/components/EmoteButton.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ButtonBase, Card, Typography } from '@mui/material';
+import { Grid } from '@mui/material';
+import { Box } from '@mui/material';
+
+function EmoteButton({imgSrc, imgName, onClickEmote}) {
+
+    let onClick = () => {
+        console.log("Sending data!")
+        onClickEmote(imgSrc, imgName)
+    }
+
+    console.log(imgSrc)
+    console.log(imgName)
+    return (
+        <ButtonBase sx = {{maxHeight: "200px", margin: .5}} onClick = {onClick}>
+            <Card>
+                <Grid>
+                    <Box height = "60%" m = {2} mb = {2}>
+                        <img alt = "" src = { imgSrc } className = "buttonImage" width = "100px" height = "100px"></img>
+                    </Box>
+                    <Box height = "40%" mt = {1} mb = {2}>
+                        <Typography> {imgName} </Typography>
+                    </Box>
+                </Grid>
+            </Card>
+        </ButtonBase>
+    );
+}
+
+export default EmoteButton;
