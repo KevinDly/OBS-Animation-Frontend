b4d65cb1e80b7a56dabecc5c0d8452263521122d
unknown <kdolay@ucsd.edu>
2023-06-08 12:38:19 -0700

    Pushed most of the emote update logic into new functions, updated support for multiple emote sources

--- src/utils/emoteCategoryUtils.js
+++ src/utils/emoteCategoryUtils.js
@@ -3,24 +3,18 @@ function mapEmotes(data){
     imgName: data.name }
 }
 
-
-function updateCategory(category, source, emotes) {
-    console.log(category)
-    console.log(source)
-    console.log("Updating category")
-    return {...category, 
-        [source] : {
-            data: emotes.data.map(emoteData => mapEmotes(emoteData))}}
+function formatEmoteData(emotes) {
+    return { data: emotes.data.map(emoteData => mapEmotes(emoteData)) }
 }
 
-/*Function that grabs the emotes from a connected api.
-component is the component that will store the emote information. component must have a state called emoteCategories that is related to a dict of sources: emotes.
-source is a string describing the origin of the emotes. This source will be displayed on the webpage.
-emotes is a dict of emotes, containing a parameter known as data. data is an array that has emote information. Particularly an image source and an image name. */
-
-export function updateEmoteCategories(component, source, emotes) {
-    if(!(source in Object.keys(component.state.emoteCategories))) {
-        let updatedEmoteCategory = updateCategory(component.state.emoteCategories, source, emotes)
-        component.setState({ emoteCategories: updatedEmoteCategory })
-    }
+/*Function that returns an updated version of the categories dict.
+currentCategories: The dict to be updated.
+recievedCategories: The emotes to add to the dict.*/
+export function updateCategories(currentCategories, recievedCategories) {
+    let updatedCategories = {...currentCategories}
+    Object.keys(recievedCategories).forEach((key) => {
+        if(!(key in updatedCategories))
+            updatedCategories[key] = formatEmoteData(recievedCategories[key])
+    })
+    return updatedCategories
 }
