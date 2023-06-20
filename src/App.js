16d9ffb398ba194f4b207652fcf507cbf21c01fb
unknown <kdolay@ucsd.edu>
2023-06-20 12:48:59 -0700

    Added automatic client reconnect, removed unnecessary state data, changed soundcheckbox so it doesnt trigger a state change

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import "./App.css"
 import { updateCategories } from './utils/emoteCategoryUtils.js'
 import SourceContainer from './components/SourceContainer.js'
-import { Stack
- } from '@mui/material';
+import { Checkbox } from '@mui/material';
+
+const DATA_SEND_TYPE = "executeAnimation"
 const WEBSOCKET_URL = "ws://localhost:2999"
 const WEBSOCKET_PROTOCOLS = ["streamerController"]
-const DATA_SEND_TYPE = "executeAnimation"
+const WEBSOCKET_RECONNECT_TIMEOUT = 5000
 
 const emoteCategories = {
         "Built-In": {
@@ -27,45 +28,61 @@ const sounds = [
         display: "https://i.kym-cdn.com/photos/images/original/001/923/849/90f"
     },
     {
-        id: "AYAYA_Sound2",
-        src: "https://cdn.discordapp.com/attachments/319692273556258816/1116040911575588914/AYAYA_AYAYA_-_Sound_Effect_HD.mp3",
-        name: "AYAYA2",
-        display: "https://i.kym-cdn.com/photos/images/original/001/923/849/90f"
+        id: "SVAROG_Sound",
+        src: "https://cdn.discordapp.com/attachments/211664172037963776/1119006578763382804/help-me-mr.svarog-by-voicemod.mp3",
+        name: "SVAROG",
+        display: "https://progameguides.com/wp-content/uploads/2023/04/Honkai_Star_Rail_Clara_1.jpg?fit=900%2C506"
     },
-    {
-        id: "AYAYA_Sound3",
-        src: "https://cdn.discordapp.com/attachments/319692273556258816/1116040911575588914/AYAYA_AYAYA_-_Sound_Effect_HD.mp3",
-        name: "AYAYA3",
-        display: "https://i.kym-cdn.com/photos/images/original/001/923/849/90f"
-    }
 ]
+
+let websocket;
+
+//Function that manages websocket connection and related events.
+function connectWebsocket(component) {
+    websocket = new WebSocket(WEBSOCKET_URL, WEBSOCKET_PROTOCOLS)
+
+    websocket.onmessage = (event) => {
+        const msg = JSON.parse(event.data)
+        const data = msg.data
+        const type = msg.type
+        console.log("Recieved Message")
+        console.log(msg)
+        switch(type) {
+            case "recievedEmotes":
+                console.log(data)
+                try {
+                    const updatedCategories = updateCategories(component.state.emoteCategories, data)
+                    component.setState({ emoteCategories: updatedCategories })
+                }
+                catch(error) {
+                    console.log(error)
+                }
+                break
+            default:
+                break
+        }
+    }
+
+    websocket.onclose = () => {
+        setTimeout(() => connectWebsocket(component), WEBSOCKET_RECONNECT_TIMEOUT)
+    }
+}
 
 class App extends Component {
 
     constructor(props) {
         super(props)
-        this.websocket = new WebSocket(WEBSOCKET_URL, WEBSOCKET_PROTOCOLS)
+        //this.websocket = new WebSocket(WEBSOCKET_URL, WEBSOCKET_PROTOCOLS)
         
         this.sendData = this.sendData.bind(this)
         this.updateData = this.filterEmotes.bind(this)
-        this.sendEmoteButton = this.sendEmoteButton.bind(this)
         this.addData = this.addData.bind(this)
         this.removeData = this.removeData.bind(this)
-        this.enableSound = this.enableSound.bind(this)
         this.clearEmotes = this.clearEmotes.bind(this)
 
         this.state = {
-            didConnect: {},
-            didAuthenticate: {},
-
             emoteCategories: emoteCategories,
-            pickedEmoteIDs: new Set(),
-            pickedEmotes: {},
-
-            soundEnabled: false,
             sounds: sounds,
-            pickedSoundIDs: new Set(),
-            pickedSounds: {},
 
             picked: {
                 "emote": {
@@ -80,69 +97,56 @@ class App extends Component {
         }
     }
 
-    //TODO: Check if there are multiples of emotes?
-
-
     componentDidMount(){
         console.log("Mounted")
-        //connectTwitch(this, "Twitch.tv")
-        this.websocket.onmessage = (event) => {
-            const msg = JSON.parse(event.data)
-            const data = msg.data
-            const type = msg.type
-            console.log("Recieved Message")
-            console.log(msg)
-            switch(type) {
-                case "recievedEmotes":
-                    console.log(data)
-                    try {
-                        const updatedCategories = updateCategories(this.state.emoteCategories, data)
-                        this.setState({ emoteCategories: updatedCategories })
-                    }
-                    catch(error) {
-                        console.log(error)
-                    }
-                    break
-                default:
-                    break
-            }
-        }
+        connectWebsocket(this)
     }
 
     sendData() {
-        const pickedEmotes = this.state.pickedEmotes
-        const emoteDensity = document.getElementById("emoteDensityInput").value
+        const pickedEmotes = this.state.picked["emote"].pickedData
+        const pickedSounds = this.state.picked["sound"].pickedData
         const pickedEmoteKeys = Object.keys(pickedEmotes)
-        const soundEnabled = this.state.soundEnabled
+        const pickedSoundKeys = Object.keys(pickedSounds)
 
-        console.log("soundEnabled: " + soundEnabled)
+        const emoteDensity = document.getElementById("emoteDensityInput").value
+        const soundEnabled = document.getElementById("audioCheckbox").checked
 
+        console.log("Sending sounds to server.")
         if(pickedEmoteKeys.length === 0) {
-            console.log("No emotes picked!")
+            alert("No emotes have been picked.")
             return
         }
 
         if(emoteDensity <= 0) {
-            console.log("Positive number of emotes required.")
+            alert("Positive number of emotes required.")
             return
         }
 
-        const emoteURLs = pickedEmoteKeys.map(key => pickedEmotes[key])
-        const emoteData = {
+        const emoteURLs = pickedEmoteKeys.map(key => pickedEmotes[key].src)
+        const soundURLs = pickedSoundKeys.map(key => pickedSounds[key].src)
+
+        const sentData = {
             type: DATA_SEND_TYPE,
-            data: { 
-                emotes: emoteURLs,
-                emoteDensity: emoteDensity,
-                soundEnabled: soundEnabled
+            data: {
+                "emote": {
+                    emotes: emoteURLs,
+                    emoteDensity: emoteDensity
+                },
+                "sound": {
+                    sounds: soundURLs,
+                    soundEnabled: soundEnabled
+                }
             }
         }
 
-        console.log(emoteData)
+        console.log(sentData)
         try{
-            console.log(this.websocket)
-            const stringifiedData = JSON.stringify(emoteData)
+            console.log(websocket)
+            console.log("Sending data")
+            console.log(sentData)
+            const stringifiedData = JSON.stringify(sentData)
             console.log(stringifiedData)
-            this.websocket.send(stringifiedData)
+            websocket.send(stringifiedData)
         }
         catch(error) {
             console.log("Error occurred.")
@@ -150,35 +154,8 @@ class App extends Component {
         }
     }
 
-    sendEmoteButton(src, name) {
-        console.log(src)
-        const emoteData = {
-            type: DATA_SEND_TYPE,
-            data: {
-                url: src,
-                emoteDensity: document.getElementById("emoteDensityInput").value
-            }
-        }
-
-        console.log(emoteData)
-        try{
-            console.log(this.websocket)
-            const stringifiedData = JSON.stringify(emoteData)
-            console.log(stringifiedData)
-            this.websocket.send(stringifiedData)
-        }
-        catch(error) {
-            console.log(error)
-        }
-    }
-
     filterEmotes(e) {
         this.setState({filter: e.target.value})
-    }
-
-    enableSound() {
-        const previousState = !this.state.soundEnabled
-        this.setState({soundEnabled: previousState})
     }
 
     addData(data, buttonSource) {
@@ -232,7 +209,7 @@ class App extends Component {
             <input type="number" id="emoteDensityInput"></input>
             <button type="button" id="emoteDataSubmit" onClick={ this.sendData }>Submit</button>
             <button type="button" id="emoteDataSubmit" onClick={ this.clearEmotes }>Clear Emotes</button>
-            <input type="checkbox" id="audioCheckbox" name="audioCheckbox" onClick={ this.enableSound }/>
+            <Checkbox label = "Enable Sound" id = "audioCheckbox" name="audioCheckbox"/>
             <label htmlFor="audioCheckbox">Enable Sound</label>
             <SourceContainer sounds = { this.state.sounds } emoteCategories={ this.state.emoteCategories } 
                 pickedSounds = { this.state.picked["sound"].pickedData } pickedEmotes = { this.state.picked["emote"].pickedData } 
